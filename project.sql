1ad3da1490eea581376abb6a7e5a8164a674ef0e
samuelphi9991 <samuelcphillips@icloud.com>
2024-09-27 12:21:35 -0400

    .

--- project.sql
+++ project.sql
@@ -10,7 +10,8 @@ DROP TABLE IF EXISTS last_rental_time_summary;
 CREATE TABLE last_rental_time_summary (
     customer_id SMALLINT,
     rental_id INTEGER,
-    days_since_last_rental SMALLINT
+    days_since_last_rental SMALLINT,
+    PRIMARY KEY (customer_id, rental_id)
 );
 
 -- Create LastRentalTimeDetailed table
@@ -24,8 +25,20 @@ CREATE TABLE last_rental_time_detailed (
     rental_date TIMESTAMP,
     return_date TIMESTAMP,
     days_since_last_rental SMALLINT,
-    last_update TIMESTAMP
+    PRIMARY KEY (customer_id, rental_id)
 );
+
+-- Calculate number of days since rental's timestamp
+
+CREATE OR REPLACE FUNCTION calculate_days(rental_date TIMESTAMP)
+RETURNS SMALLINT AS $$
+DECLARE
+    days_difference SMALLINT;
+BEGIN
+    days_difference := CAST(CURRENT_DATE - rental_date::DATE AS SMALLINT);
+    RETURN days_difference;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Insert data into LastRentalTimeSummary table
 
@@ -62,8 +75,6 @@ SELECT
     r.rental_date,
     r.return_date,
     calculate_days(rental_date)
-    --EXTRACT(DAY FROM CURRENT_DATE - r.rental_date)::INTEGER AS days_since_last_rental
-    --CURRENT_DATE AS last_update -- edited this so that last_update field is only updated after calling trigger
 FROM
     customer c
 JOIN
@@ -75,25 +86,58 @@ WHERE
         WHERE r2.customer_id = c.customer_id
     );
 
--- Create trigger that updates last_rental_time_detailed last_update field with present time
-/*
-CREATE TRIGGER update_last_update AFTER INSERT ON last_rental_time_detailed
-FOR EACH ROW
-BEGIN
-    UPDATE last_rental_time_detailed
-    SET last_update = CURRENT_DATE
-    WHERE customer_id = NEW.customer_id;
-END
-*/
--- Create custom transformation that formats raw timestamp difference into number of days
+-- Trigger function 
 
-CREATE OR REPLACE FUNCTION calculate_days(rental_date TIMESTAMP)
-RETURNS SMALLINT AS $$
-DECLARE
-    days_difference SMALLINT;
+CREATE OR REPLACE FUNCTION copy_changes_to_summary()
+RETURNS TRIGGER AS $$
 BEGIN
-    -- Calculate the number of days since the rental_date (ignoring time part)
-    days_difference := CAST(CURRENT_DATE - rental_date::DATE AS SMALLINT);
-    RETURN days_difference;
+    INSERT INTO last_rental_time_summary (customer_id, rental_id, days_since_last_rental)
+    VALUES (NEW.customer_id, NEW.rental_id, NEW.days_since_last_rental)
+    ON CONFLICT (customer_id, rental_id) -- If the row already exists, update it
+    DO UPDATE SET
+        days_since_last_rental = EXCLUDED.days_since_last_rental;
+
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+
+-- Clear both tables' data and insert original data from dvdrental database
+CREATE OR REPLACE PROCEDURE refresh_data() AS $$
+BEGIN 
+    DROP TABLE last_rental_time_summary;
+    DROP TABLE last_rental_time_detailed;
+        INSERT INTO last_rental_time_detailed (customer_id, store_id, create_date, rental_id, rental_date, return_date, days_since_last_rental)
+    SELECT
+        c.customer_id,
+        c.store_id,
+        c.create_date,
+        r.rental_id,
+        r.rental_date,
+        r.return_date,
+        calculate_days(rental_date)
+        --EXTRACT(DAY FROM CURRENT_DATE - r.rental_date)::INTEGER AS days_since_last_rental
+        --CURRENT_DATE AS last_update -- edited this so that last_update field is only updated after calling trigger
+    FROM
+        customer c
+    JOIN
+        rental r ON c.customer_id = r.customer_id
+    WHERE
+        r.rental_date = (
+            SELECT MAX(r2.rental_date)
+            FROM rental r2
+            WHERE r2.customer_id = c.customer_id
+        );
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER copy_changes_summary
+AFTER INSERT OR UPDATE ON last_rental_time_detailed
+FOR EACH ROW
+EXECUTE FUNCTION copy_changes_to_summary();
+
+DROP TRIGGER IF EXISTS copy_changes_summary ON last_rental_time_detailed;
+DROP FUNCTION IF EXISTS copy_changes_to_summary();
+DROP FUNCTION IF EXISTS calculate_days;
+DROP TABLE IF EXISTS last_rental_time_summary;
+DROP TABLE IF EXISTS last_rental_time_detailed;
+DROP PROCEDURE IF EXISTS refresh_data;
